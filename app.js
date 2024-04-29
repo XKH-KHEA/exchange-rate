5442eef6fc0bad819362043880b1bffe0cfbfa79
onkhea <khea@xtricate.com>
2024-04-29 15:04:32 +0700

    api

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   const date = req.query.date ?? "";
-  
+
   scrapeNBC(date)
     .then(function (data) {
       res.setHeader("Content-Type", "text/plain");
@@ -58,7 +58,7 @@ async function scrapeNBC(date) {
   return data;
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
