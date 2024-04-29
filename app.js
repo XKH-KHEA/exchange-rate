d29c901709f660fd551927758452ea1134128080
onkhea <khea@xtricate.com>
2024-04-29 15:01:40 +0700

    api

--- app.js
+++ app.js
@@ -1,171 +1,64 @@
-// // const express = require("express");
-// // const puppeteer = require("puppeteer"); // Use puppeteer-core
-// // const cheerio = require("cheerio");
-// // const cors = require("cors");
-
-// // const app = express();
-// // app.use(cors());
-
-// // app.get("", async (req, res) => {
-// //   try {
-// //     const today = new Date().toISOString().split("T")[0];
-// //     const dateFilter = req.query.date || today;
-
-// //     const browser = await puppeteer.launch({
-// //       headless: "new",
-// //       executablePath:
-// //         "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-// //     });
-
-// //     const page = await browser.newPage();
-
-// //     await page.goto(
-// //       "https://www.nbc.gov.kh/english/economic_research/exchange_rate.php"
-// //     );
-// //     await page.waitForTimeout(2000);
-
-// //     await page.$eval(
-// //       "#datepicker",
-// //       (datepicker, dateFilter) => {
-// //         datepicker.value = dateFilter;
-// //       },
-// //       dateFilter
-// //     );
-
-// //     await page.click('input[name="view"]');
-// //     await page.waitForTimeout(2000);
-
-// //     const content = await page.content();
-// //     const $ = cheerio.load(content);
-
-// //     const exchangeRates = [];
-
-// //     $("table.tbl-responsive tr").each((index, element) => {
-// //       if (index > 0) {
-// //         const columns = $(element).find("td");
-// //         const currency = columns.eq(0).text().trim();
-// //         const Symbol = columns.eq(1).text().trim();
-// //         const unit = columns.eq(2).text().trim();
-// //         const bid = columns.eq(3).text().trim();
-// //         const ask = columns.eq(4).text().trim();
-
-// //         exchangeRates.push({ currency, Symbol, unit, bid, ask });
-// //       }
-// //     });
-
-// //     const officialExchangeRateRow = $('td:contains("Official Exchange Rate")');
-// //     const officialExchangeRateText = officialExchangeRateRow.text();
-// //     const officialExchangeRateMatch = officialExchangeRateText.match(/(\d+)/);
-// //     const officialExchangeRate = officialExchangeRateMatch
-// //       ? parseInt(officialExchangeRateMatch[1])
-// //       : null;
-
-// //     await browser.close();
-
-// //     const response = {
-// //       ok: true,
-// //       value: exchangeRates,
-// //       officialExchangeRate,
-// //       date: dateFilter,
-// //     };
-
-// //     res.json(response);
-// //   } catch (error) {
-// //     console.error("Error:", error);
-
-// //     if (error instanceof puppeteer.errors.TimeoutError) {
-// //       res.status(500).json({ error: "Timeout Error" });
-// //     } else {
-// //       res.status(500).json({ error: "Internal Server Error" });
-// //     }
-// //   }
-// // });
-// // const PORT = 5000;
-// // app.listen(process.PORT, () => {
-// //   console.log(`http://localhost:${PORT}`);
-// // });
-
 const express = require("express");
-const puppeteer = require("puppeteer"); // Use puppeteer-core
-const cheerio = require("cheerio");
-const cors = require("cors");
-
+const puppeteer = require("puppeteer");
+const dotenv = require("dotenv").config();
 const app = express();
-app.use(cors());
 
-app.get("/", async (req, res) => {
-  // Changed the route from "" to "/"
-  try {
-    const today = new Date().toISOString().split("T")[0];
-    const dateFilter = req.query.date || "2024-04-25";
-
-    const browser = await puppeteer.launch({ headless: true });
-
-    const page = await browser.newPage();
-
-    await page.goto(
-      "https://www.nbc.gov.kh/english/economic_research/exchange_rate.php"
-    );
-    await page.waitForTimeout(2000);
-
-    await page.$eval(
-      "#datepicker",
-      (datepicker, dateFilter) => {
-        datepicker.value = dateFilter;
-      },
-      dateFilter
-    );
-
-    await page.click('input[name="view"]');
-    await page.waitForTimeout(2000);
-
-    const content = await page.content();
-    const $ = cheerio.load(content);
-
-    const exchangeRates = [];
-
-    $("table.tbl-responsive tr").each((index, element) => {
-      if (index > 0) {
-        const columns = $(element).find("td");
-        const currency = columns.eq(0).text().trim();
-        const Symbol = columns.eq(1).text().trim();
-        const unit = columns.eq(2).text().trim();
-        const bid = columns.eq(3).text().trim();
-        const ask = columns.eq(4).text().trim();
-
-        exchangeRates.push({ currency, Symbol, unit, bid, ask });
-      }
+app.get("/", (req, res) => {
+  const date = req.query.date ?? "";
+  
+  scrapeNBC(date)
+    .then(function (data) {
+      res.setHeader("Content-Type", "text/plain");
+      res.send(data);
+    })
+    .catch(function (e) {
+      res.status(500, {
+        error: e,
+      });
     });
-
-    const officialExchangeRateRow = $('td:contains("Official Exchange Rate")');
-    const officialExchangeRateText = officialExchangeRateRow.text();
-    const officialExchangeRateMatch = officialExchangeRateText.match(/(\d+)/);
-    const officialExchangeRate = officialExchangeRateMatch
-      ? parseInt(officialExchangeRateMatch[1])
-      : null;
-
-    await browser.close();
-
-    const response = {
-      ok: true,
-      value: exchangeRates,
-      officialExchangeRate,
-      date: dateFilter,
-    };
-
-    res.json(response);
-  } catch (error) {
-    console.error("Error:", error);
-
-    if (error instanceof puppeteer.errors.TimeoutError) {
-      res.status(500).json({ error: "Timeout Error" });
-    } else {
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
 });
 
-const PORT = process.env.PORT || 5000; // Changed process.PORT to process.env.PORT
+async function scrapeNBC(date) {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto(
+    "https://www.nbc.gov.kh/english/economic_research/exchange_rate.php",
+    { waitUntil: "domcontentloaded" }
+  );
+
+  await page.focus("#datepicker");
+  await page.keyboard.down("Control");
+  await page.keyboard.press("A");
+  await page.keyboard.up("Control");
+  await page.keyboard.press("Backspace");
+  await page.keyboard.type(date);
+  await page.click('input[type="submit"]');
+
+  let data = await page.evaluate(() => {
+    let date = document.querySelector(
+      "#fm-ex > table > tbody > tr:nth-child(1) > td > font"
+    ).innerText;
+    let rate = document.querySelector(
+      "#fm-ex > table > tbody > tr:nth-child(2) > td > font"
+    ).innerText;
+    return {
+      exchange_date: date,
+      exchange_rate: rate,
+      currency,
+      Symbol,
+      unit,
+      bid,
+      ask,
+    };
+  });
+  await browser.close();
+  return data;
+}
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
